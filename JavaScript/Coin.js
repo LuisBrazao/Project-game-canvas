459a0fab62646fe09849deb38619fce3c6ba90dc
Luis Brazao <luis.ms.brazao@gmail.com>
2020-10-27 18:55:32 +0000

    support for githubpages and more fluid coin animation

--- JavaScript/Coin.js
+++ JavaScript/Coin.js
@@ -19,41 +19,73 @@ class Coin {
         switch (this.currentFrame) {
             case 1:
                 this.image.src = './images/Gold/Gold_2.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 2:
                 this.image.src = './images/Gold/Gold_3.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 3:
                 this.image.src = './images/Gold/Gold_4.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 4:
                 this.image.src = './images/Gold/Gold_5.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 5:
                 this.image.src = './images/Gold/Gold_6.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 6:
                 this.image.src = './images/Gold/Gold_7.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 7:
                 this.image.src = './images/Gold/Gold_8.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 8:
                 this.image.src = './images/Gold/Gold_9.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 9:
                 this.image.src = './images/Gold/Gold_10.png';
-                this.currentFrame ++;
+                this.currentFrame++;
                 break;
             case 10:
+                this.image.src = './images/Gold/Gold_9.png';
+                this.currentFrame++;
+                break;
+            case 11:
+                this.image.src = './images/Gold/Gold_10.png';
+                this.currentFrame++;
+                break;
+            case 12:
+                this.image.src = './images/Gold/Gold_7.png';
+                this.currentFrame++;
+                break;
+            case 13:
+                this.image.src = './images/Gold/Gold_6.png';
+                this.currentFrame++;
+                break;
+            case 14:
+                this.image.src = './images/Gold/Gold_5.png';
+                this.currentFrame++;
+                break;
+            case 15:
+                this.image.src = './images/Gold/Gold_4.png';
+                this.currentFrame++;
+                break;
+            case 16:
+                this.image.src = './images/Gold/Gold_3.png';
+                this.currentFrame++;
+                break;
+            case 17:
+                this.image.src = './images/Gold/Gold_2.png';
+                this.currentFrame++;
+                break;
+            case 18:
                 this.image.src = './images/Gold/Gold_1.png';
                 this.currentFrame = 1;
                 break;
